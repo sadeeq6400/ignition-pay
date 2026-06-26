dbaac4781008bbfb29150300cf9875dcaf4fb9eb
danieloche635-bit <danieloche635@gmail.com>
2026-06-26 12:46:54 +0100

    feat: add dart wallet example

--- examples/dart-wallet/basic-wallet/main.dart
+++ examples/dart-wallet/basic-wallet/main.dart
@@ -1,25 +1,19 @@
 import 'package:stellar/stellar.dart';
 
 void main() async {
-  // Generate a new keypair
   final keypair = Keypair.random();
-  print('Public Key: ${keypair.accountId}');
-  print('Secret Seed: ${keypair.secretSeed}');
+  print('Public Key: \');
+  print('Secret Seed: \');
 
-  // Connect to Horizon testnet
   final server = Server('https://horizon-testnet.stellar.org');
-
-  // Create account
   await FriendBot.fundTestAccount(keypair.accountId);
   print('Account funded on testnet');
 
-  // Check balance
   final account = await server.accounts.account(keypair.accountId);
   for (final balance in account.balances) {
-    print('Balance: ${balance.balance} ${balance.assetType}');
+    print('Balance: \ \');
   }
 
-  // Send a payment
   final destination = 'GBSH7WNSDU5K3J2I5H3K2M6QZQKJ3PZ7PJ3VJ6QZQKJ3PZ7PJ3VJ6';
   final transaction = TransactionBuilder(account)
     .addOperation(PaymentOperation(
@@ -31,5 +25,5 @@ void main() async {
 
   transaction.sign(keypair);
   final response = await server.submitTransaction(transaction);
-  print('Transaction hash: ${response.hash}');
+  print('Transaction hash: \');
 }
